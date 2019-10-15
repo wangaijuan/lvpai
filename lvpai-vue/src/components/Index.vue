c293b5c6a7705e522a1e5481f79f94f3148ee643
bzz <bzz@163.com>
2019-10-15 17:53:04 +0800

    新建Index.vue用于测试

---
+++ lvpai-vue/src/components/Index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+  
+    <van-button type="info">蓝色按钮</van-button><br><br>
+    <mt-button type="danger">红色按钮</mt-button>
+  </div>
+</template>
--- lvpai-vue/src/router.js
+++ lvpai-vue/src/router.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+//测试 引入了Index.vue文件 
+import Index from "./components/Index.vue"
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    //测试:引入index.vue
+    {path:"/Index",component:Index},
     {
       path: '/',
       name: 'home',
