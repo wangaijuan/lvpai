c1583ea63f750afc064c56020097607710a863e7
bzz <bzz@163.com>
2019-10-17 20:47:57 +0800

    Home

--- lvpai-vue/src/App.vue
+++ lvpai-vue/src/App.vue
@@ -17,7 +17,7 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  padding: 5px;
 }
 
 #nav a {
--- lvpai-vue/src/components/HomePage.vue
+++ lvpai-vue/src/components/HomePage.vue
@@ -2,48 +2,53 @@
 <!-- 最外层父元素 -->
 <div class="parent">
   <!--搜素框 -->
-  <div class="input">
-     <van-search placeholder="请输入搜索关键词" v-model="value" />
-  </div> 
-    <!-- 小图标 -->
-    <div >
-      <a href="#">
-          <img class="icon" src="../assets/small/phone.png" />
+    <div class="input">
+      <p>西安</p >
+      <van-search placeholder="DIY个性化婚礼"  />
+      <!-- 小图标 -->
+      <a href="#" class="a1">
+        <img class="icon" src="../assets/small/phone.png" />
+      </a>
+      <a href="#"  class="a1">
+        <img class="icon" src="../assets/small/message.png" />
+      </a> 
+    </div> 
+
+    <!-- 轮播图 -->
+    <div class="swipe">
+      <van-swipe :autoplay="3000" indicator-color="white">
+        <van-swipe-item>
+        <a href="#"> 
+          <img class="swipe" src="../assets/home/focus1.jpg" />
         </a>
-        <a href="#">
-          <img class="icon" src="../assets/small/message.png" />
-        </a>
-  
-  </div>
+        </van-swipe-item>
+        <van-swipe-item>
+          <a href="#">
+          <img class="swipe" src="../assets/home/focus2.jpg" />
+          </a>
+        </van-swipe-item>
+        <van-swipe-item>
+          <a href="#">
+          <img class="swipe"  src="../assets/home/focus3.jpg" />
+          </a>
+        </van-swipe-item>
+        <van-swipe-item>
+          <a href="#">
+          <img class="swipe" src="../assets/home/focus4.jpg" />
+          </a>
+        </van-swipe-item>
+      </van-swipe>
+    </div>
  
-      
-  <!-- 轮播图 -->
-  <div class="swipe">
-    <van-swipe :autoplay="3000" indicator-color="white">
-      <van-swipe-item>
-       <a href="#"> 
-         <img class="swipe" src="../assets/home/focus1.jpg" />
-       </a>
-      </van-swipe-item>
-      <van-swipe-item>
-        <a href="#">
-         <img class="swipe" src="../assets/home/focus2.jpg" />
-        </a>
-      </van-swipe-item>
-      <van-swipe-item>
-        <a href="#">
-         <img class="swipe"  src="../assets/home/focus3.jpg" />
-        </a>
-      </van-swipe-item>
-      <van-swipe-item>
-        <a href="#">
-         <img class="swipe" src="../assets/home/focus4.jpg" />
-        </a>
-      </van-swipe-item>
-    </van-swipe>
-  </div>
- </div> 
+</div>
+ 
 </template>
+
+<script>
+    export default{
+
+    }
+</script>
 <style scoped>
 /* 轮播图形样式 */
     .swipe{
@@ -51,17 +56,22 @@
     }
     /* 搜素框图形样式 */
      .input{
-     
       display: flex;
     }
-      /*
-      搜索框内边距 
-    .input input{
-      padding:0.5rem;
-    } */
+
     .icon{
-      width:8%;
+      width:30%;
       /* margin-top:0.1rem; */
+      padding:18px;
        }
+    .sp1{
+      width:200px;
+      height:5px;
+      margin-top:100px;
+    }
+    .a1{
+      width:100px;
+      height:5px;
+    }
   
 </style>
